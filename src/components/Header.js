2c9e90b081973f76e594bd7fa05ad7b10b12daa2
Christabelle143 <christabelleyouta143@gmail.com>
2022-07-13 13:00:21 +0100

    create todo-app

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => (
+  <header className="header">
+    <h1>Todo App</h1>
+    <p>Add todo item and mark as completed or delete it when done.</p>
+  </header>
+);
+
+export default Header;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from "./App";
+// importing component files.
+import TodoContainer from './components/TodoContainer';
+
+ReactDOM.render(<TodoContainer />, document.getElementById('root'));
